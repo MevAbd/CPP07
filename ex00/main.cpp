ee44084e1ef618380cf55eced453d4cff4dcb1c0
Administrateur <maeva.albrand@icloud.com>
2022-07-17 19:06:36 +0200

    ex00 makefile free

---
+++ ex00/main.cpp
@@ -0,0 +1,39 @@
+#include "whatever.hpp"
+
+int	main( void )
+{
+	std::cout << "*********************TESTS SUBJECT*********************" <<  std::endl;
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::cout << std::endl << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << std::endl << std::endl;
+	std::cout << "*********************OTHER TEST*********************" <<  std::endl;
+	char	e = 'a';
+	char	j = 'b';
+	double	i = 1.1;
+	double	o = 4.2;
+	float	k = 0.3f;
+	float	p = -4.2f;
+	std::cout << "e = " << e << ", j = " << j << std::endl;
+	std::cout << "min( e, j ) = " << ::min( e, j ) << std::endl;
+	std::cout << "max( e, j ) = " << ::max( e, j ) << std::endl;
+	std::cout << std::endl << std::endl;
+	std::cout << "i = " << i << ", o = " << o << std::endl;
+	std::cout << "min( i, o ) = " << ::min( i, o ) << std::endl;
+	std::cout << "max( i, o ) = " << ::max( i, o ) << std::endl;
+	std::cout << std::endl << std::endl;
+	std::cout << "k = " << k << ", p = " << p << std::endl;
+	std::cout << "min( k, p ) = " << ::min( k, p ) << std::endl;
+	std::cout << "max( k, p ) = " << ::max( k, p ) << std::endl;
+	return 0;
+}
---
+++ ex00/whatever.hpp
@@ -0,0 +1,25 @@
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+# include <iostream>
+
+template< typename T> T max(T const & x, T const & y)
+{
+	return ((x>y) ? x : y);
+}
+
+template< typename T> T min(T const & x, T const & y)
+{
+	return ((x<y) ? x : y);
+}
+
+template<typename T> void swap(T & x, T & y)
+{
+	T	cpy;
+
+	cpy = x;
+	x = y;
+	y = cpy;
+}
+
+#endif
