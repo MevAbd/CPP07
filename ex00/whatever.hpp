e9ae749e9a4337f38f6b9afcf8c789b4f4b2e06f
Maeva ALBRAND <malbrand@e1r1p5.clusters.42paris.fr>
2022-07-19 00:41:12 +0200

    add Makefile

--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -1,8 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malbrand <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/19 00:37:49 by malbrand          #+#    #+#             */
+/*   Updated: 2022/07/19 00:37:51 by malbrand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef WHATEVER_HPP
 # define WHATEVER_HPP
 
 # include <iostream>
-
 template< typename T> T max(T const & x, T const & y)
 {
 	return ((x>y) ? x : y);
