bf720bc0c9c41f13b8a005e610c1f967585c1334
Timothé Gré <timothegre@MacBook-Pro-de-Timothe.local>
2022-07-18 19:59:57 +0200

    ex 01 free Makefile

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -3,12 +3,25 @@
 
 # include <iostream>
 
-template< typename T > void iter(T *array, size_t len, void ctfct)(T &))
+template< typename T > void iter(T *arr, const size_t len, void (*f)(T &))
 {
-	if (array == NULL || fct == NULL)
+	if (arr == NULL || f == NULL)
 		return ;
 	for (size_t i = 0; i < len; i++)
-		fct(array[i]);
+		f(arr[i]);
+}
+
+template< typename T > void iter(const T *arr, const size_t len, void (*f)(const T &))
+{
+	if (arr == NULL || f == NULL)
+		return ;
+	for (size_t i = 0; i < len; i++)
+		f(arr[i]);
+}
+
+template<typename T> void	printData(const T &arg)
+{
+	std::cout << arg << " ";
 }
 
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,16 +1,45 @@
 #include "iter.hpp"
 
-void	incrementation(int *tab)
+void	upChar(char &arg)
 {
-	*tab = *tab + 1;
+	arg = arg - 32;
 }
 
 
+void	downInt(int &arg)
+{
+	arg--;;
+}
+
+void	divFloat(float &arg)
+{
+	arg = arg / 10;;
+}
+
 int main(void)
 {
-	int	tab[5] = {0, 1, 2, 3, 4};
+	std::cout << "**************************************CHAR*******************************" << std::endl;
+	char	tab[5] = {'a', 'b', 'c', 'd', 'e'};
 
-	iter(tab, 5, incrementation);
-	std::cout << tab[0] << " " << tab[1] << " " << tab[2] << " " << std::endl;
+	iter(tab, 5, printData);
+	std::cout << std::endl;
+	iter(tab, 5, upChar);
+	iter(tab, 5, printData);
+	std::cout << std::endl;
+	std::cout << "**************************************INT*******************************" << std::endl;
+	int	tab2[5] = {0, 1, 2, 3, 4};
+
+	iter(tab2, 5, printData);
+	std::cout << std::endl;
+	iter(tab2, 5, downInt);
+	iter(tab2, 5, printData);
+	std::cout << std::endl;
+	std::cout << "**************************************FLOAT*******************************" << std::endl;
+	float	tab3[5] = {4.2, 8.4, 16.8, 33.0, 66.2};
+
+	iter(tab3, 5, printData);
+	std::cout << std::endl;
+	iter(tab3, 5, divFloat);
+	iter(tab3, 5, printData);
 	return (0);
 }
